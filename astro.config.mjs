dd119bf45b7de454852393a18c61225f7f57df42
Nahuel Gonzalez <cryptonahue@gmail.com>
2025-08-29 12:54:29 -0300

    fix: deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/serverless';
+// import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: vercel(),
+  output: 'static',
+  // adapter: vercel({
+  //   functionPerRoute: false,
+  //   includeFiles: []
+  // }),
   integrations: [
     tailwind(),
     react(),
